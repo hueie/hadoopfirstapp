c000738e07a3fdb8bbfab133d6b0749cd25728e0
kait <kait@kait-PC>
2017-04-10 15:09:05 +0900

    all update

--- firsthadoopapp/src/main/java/ledger/index/web/IndexController.java
+++ firsthadoopapp/src/main/java/ledger/index/web/IndexController.java
@@ -29,4 +29,27 @@ public class IndexController {
     	
     	return index;
     }
+    
+    
+    /*
+     
+    @RequestMapping(value="/{user}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Long user) {
+        // ...
+    }
+
+    @RequestMapping(value="/{user}/customers", method=RequestMethod.GET)
+    List<Customer> getUserCustomers(@PathVariable Long user) {
+        // ...
+    }
+
+    @RequestMapping(value="/{user}", method=RequestMethod.DELETE)
+    public User deleteUser(@PathVariable Long user) {
+        // ...
+    }
+
+      
+      
+     */
+    
 }
